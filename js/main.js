a810a928963e5c0b5159c2705492376b4b3c533b
Rohan shah <rohanshah10296@yahoo.com>
2018-04-06 20:58:49 +0530

    except slick

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ $j(document).ready(function() {
         infinite: false,
       
        slidesToShow: 3,
-    
+         prevArrow:'<div class="slick-prev"><button type="button" class="triangle"></button></div>',  
+         nextArrow:'<div class="slick-next"><button type="button" class="triangle"></button></div>',
         slidesToScroll: 1
     });
     
