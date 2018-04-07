e24387cf79431c0e382870d840cf0629454f6a74
Rohan shah <rohanshah10296@yahoo.com>
2018-04-07 20:19:42 +0530

    desktop-tablet

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ $j(document).ready(function() {
     $j('.clientSayList').slick({
         infinite: false,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+                 prevArrow:'<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',  
+         nextArrow:'<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
+
     });
      $j('.portfolioList').slick({
         infinite: false,
       
        slidesToShow: 3,
-         prevArrow:'<div class="slick-prev"><button type="button" class="triangle"></button></div>',  
-         nextArrow:'<div class="slick-next"><button type="button" class="triangle"></button></div>',
-        slidesToScroll: 1
+         prevArrow:'<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',  
+         nextArrow:'<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
+        slidesToScroll: 1,
+         responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+        slidesToShow:1
+    }
+  }
+ 
+  ]
     });
     
 });
