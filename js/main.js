93362b8a8fa1b991df54184776cf2089199eefb5
Sohil Shah <shahsohil25@gmail.com>
2018-04-08 14:59:55 +0530

    style and structure changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
-$j(document).ready(function() {
-    $j('.clientSayList').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-                 prevArrow:'<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',  
-         nextArrow:'<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
+$j(document).ready(function () {
+  $j('.clientSayList').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',
+    nextArrow: '<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
+  });
+  $j('.portfolioList').slick({
+    infinite: false,
+    slidesToShow: 3,
+    prevArrow: '<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',
+    nextArrow: '<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1
+        }
+      }
 
-    });
-     $j('.portfolioList').slick({
-        infinite: false,
-      
-       slidesToShow: 3,
-         prevArrow:'<div class="slick-slider slick-prev"><span type="button" class="prev"></span></div>',  
-         nextArrow:'<div class="slick-slider slick-next"><span type="button" class="next"></span></div>',
-        slidesToScroll: 1,
-         responsive: [
-  {
-    breakpoint: 1024,
-    settings: {
-        slidesToShow:1
-    }
-  }
- 
-  ]
-    });
-    
+    ]
+  });
+
 });
